cda35b7a53cc5962a4069017245460a3bb7a0934
irfan1718 <its2irfan17@gmail.com>
2023-01-22 13:48:44 +0530

    Pattern Printing

--- practice/Star.java
+++ practice/Star.java
@@ -3,8 +3,8 @@ package practice;
 public class Star {
     public static void main(String[] args) {
         for(int i=1;i<10;i++){
-            for(int j=0;j<i;j++){
-            System.out.println("* ");
+            for(int j=i;j<10;j++){
+            System.out.print("*\t");
             }
             System.out.println("\n");
         }
